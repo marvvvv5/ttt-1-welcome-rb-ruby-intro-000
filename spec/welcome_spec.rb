21762ec4be86bf718cd58960bd7b782c6cd2e1b6
Marvin Ivory II <mcivory10@gmail.com>
2021-01-14 20:33:04 +0000

    Done.

--- spec/welcome_spec.rb
+++ spec/welcome_spec.rb
@@ -1,7 +1,13 @@
 describe "lib/welcome.rb" do
   it 'prints "Welcome to Tic Tac Toe!"' do
     expect($stdout).to receive(:puts).with("Welcome to Tic Tac Toe!"), "Make sure lib/welcome.rb has code that can output 'Welcome to Tic Tac Toe!' exactly."
-    
+
     load './lib/welcome.rb'
   end
 end
+
+
+# well the file lib/welcome.rb should have something do with welcome to Tic Tac Toe!
+
+
+# 1. Say hello.
